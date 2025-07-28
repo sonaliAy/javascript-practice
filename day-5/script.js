28f27af8d9431d82d4e85d2f39a279220d67f30b
sonaliAy <sonalijaiswals2005@gmail.com>
2025-07-28 22:34:20 +0530

    day-5

---
+++ day-5/script.js
@@ -0,0 +1,29 @@
+//primitive data types:
+//String
+//Number
+//Boolean
+//undefined
+//null
+//BigInt
+//Symbol
+
+//non-primitive data types:(reference data type)
+//Array
+//function
+//object
+
+//var a = 34;
+//console.log(typeof a);
+
+//var a = null;
+//console.log(typeof a);
+
+//var a = 146574875857894;
+//console.log(typeof a );
+
+//var a =("df");
+//console.log(typeof a);
+
+//non-primitive
+var a = function abc(){};
+console.log(typeof a);
