c7bdce015adb42c7d28748d16a37057f706f501d
sonaliAy <sonalijaiswals2005@gmail.com>
2025-07-28 22:42:29 +0530

    day-6

---
+++ day-6/script.js
@@ -0,0 +1,13 @@
+let x = 5;
+let y = 4;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+console.log(x**y);  //exponational
+console.log(++x);  //increment
+console.log(--x);  //decrement
+
+
+
