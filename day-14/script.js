90a136bf09f8bd5ff73a24820da7658f23c44c89
sonaliAy <sonalijaiswals2005@gmail.com>
2025-08-01 23:02:01 +0530

    day-14

---
+++ day-14/script.js
@@ -0,0 +1,20 @@
+//let pass = " ";
+//do{
+  //  pass = prompt();
+//}
+//while (pass!=="abcd");
+
+//console.log("welcome");
+
+
+//let x = 1;
+//while(x<=5){
+   // console.log(x);
+//}
+
+let x = 1;
+do{
+    console.log(x)
+    x++;
+}
+while(x<=5)
