93317d1b6ba9d5e4472a55ac798470640c573c66
sonaliAy <sonalijaiswals2005@gmail.com>
2025-08-01 23:11:08 +0530

    day-15

---
+++ day-15/script.js
@@ -0,0 +1,13 @@
+//for(let x=1;x<=5;x++)
+//{
+   // console.log(x);
+//}
+
+for(let x=1;x<=5;x++)
+{
+    console.log("x",x);
+    for(let y=1;y<=5;y++)
+    {
+        console.log("y",y);
+    }
+}
