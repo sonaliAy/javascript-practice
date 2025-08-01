de7855774acef1f283ce2290110065eb2823d14c
sonaliAy <sonalijaiswals2005@gmail.com>
2025-08-01 22:31:20 +0530

    day-12

---
+++ day-12/script.js
@@ -0,0 +1,28 @@
+let marks = 60;
+//if (marks>=70)
+   // {
+   //     console.log("pass");
+   // } 
+   // else{
+    //    console.log("fail");
+   // }
+
+   //ternary operator
+   //let marks = 80;
+   //marks>=120?console.log("pass"):console.log("fail");
+
+   let a = 10;
+   let b = 3;
+   //if(a===3){
+      //console.log("a");
+     // if(b===1){
+    //     console.log("b");
+     // }
+   //}
+   //else{
+    //  console.log("fail");
+   //}
+
+   a===10?(console.log("a"),b===5?console.log("b"):
+   console.log("b fail"))
+  : console.log("a fail");
