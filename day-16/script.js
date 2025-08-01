0057719aaaba13c3634c77f6e8ed3832503c1e1b
sonaliAy <sonalijaiswals2005@gmail.com>
2025-08-01 23:23:46 +0530

    day-16

---
+++ day-16/script.js
@@ -0,0 +1,28 @@
+//for(let x=1;x<=10;x++)
+//{
+   // console.log(x);
+   // if(x===5)
+   // {
+       // break;
+   // }
+//}
+
+//for(let x=1;x<=10;x++)
+//{
+  //  if(x===5)
+   // {
+   //     continue;
+   // }
+   // console.log(x);
+//}
+
+
+//even or odd
+for(let x=1;x<=10;x++)
+{
+    if(x%2===0)
+    {
+        continue;
+    }
+    console.log(x);
+}
