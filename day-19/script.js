611465bc70ce4eadf9215f8c5409bed8954fe0b4
sonaliAy <sonalijaiswals2005@gmail.com>
2025-08-02 00:45:15 +0530

    day-19

---
+++ day-19/script.js
@@ -0,0 +1,9 @@
+const scores = [40,60,89,34,100,79];
+for(let i=0;i<scores.length;i++){
+    if(scores[i]>50){
+    console.log(scores[i]+" pass ");
+}
+else {
+    console.log(scores[i] +" fail")
+}
+}
